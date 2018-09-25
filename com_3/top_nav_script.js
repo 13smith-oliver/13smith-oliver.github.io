bb97863e752b050ecd1e57a8df1de5a2f62b5589
Oliver Smith <31206313+13smith-oliver@users.noreply.github.com>
2018-09-25 21:47:36 +0100

    Rearranged Site

--- com_3/top_nav_script.js
+++
@@ -1,8 +0,0 @@
-function topNavFunc() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "top-bar") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-bar";
-    }
-}
